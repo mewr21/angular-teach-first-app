cb07d238da41fabffe4104b866645f968b4f5a2d
meow <rungnapa.te@ku.th>
2019-02-10 14:34:22 +0700

    'message'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Card } from './card';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  caeds: Card[] = [];
+  get sorted() : Card[]{
+    console.log('access');
+    return this.cards.sort((a, b) => b.votes - a.votes);  //เรียงจากมากไปน้อย
+
+  }
+  cards: Card[] =[];
+  onclick(firstName :HTMLInputElement,lastName :HTMLInputElement){
+    //console.log(firstName.value,lastName.value);
+    const card = new Card(firstName.value,lastName.value,0);
+    console.log(card)
+    this.cards.push(card);
+    firstName.value ='';
+    lastName.value = '';
+    
+  }
 }
